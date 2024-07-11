bec058e8bd11c5e77d0cce072c1f5e4cbd54bf72
Antti Ruonakoski <aruonakoski@gmail.com>
2024-07-11 21:51:51 +0300

    add custom fonts

--- api/index.js
+++ api/index.js
@@ -6,7 +6,14 @@ module.exports = async (req, res) => {
         )
         .replace(
             '</HEAD>',
-            '<link media="all" href="/custom.css" rel="stylesheet" /></head>'
+            `<link media="all" href="/custom.css" rel="stylesheet" />
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap" rel="stylesheet"></link>    
+            </head >`
         )
     // 1 year
     res.setHeader('Cache-Control', 'max-age=0, s-maxage=360')
