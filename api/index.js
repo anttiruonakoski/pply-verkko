a74b50c1cf9ffd3217d222ea74c53a1147212879
Antti Ruonakoski <aruonakoski@gmail.com>
2024-07-11 21:37:06 +0300

    fix bugs

--- api/index.js
+++ api/index.js
@@ -2,14 +2,14 @@ const fetch = require('node-fetch')
 
 module.exports = async (req, res) => {
     const html = (
-        await(await fetch('http://lists.oulu.fi/pipermail/pply' + req.url)).text()
-            .replace(
-                '</head>',
-                '<link media="all" href="/custom.css" rel="stylesheet" /></head>'
-            )
+        await (await fetch('http://lists.oulu.fi/pipermail/pply/' + req.url)).text()
+        )
+        .replace(
+            '</HEAD>',
+            '<link media="all" href="/custom.css" rel="stylesheet" /></head>'
         )
     // 1 year
-    res.setHeader('Cache-Control', 'max-age=0, s-maxage=60')
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=360')
     res.send(html)
     res.end()
 }
